44ca49a15b11a65a987224919fb1f6dfd5798a7d
crh-jcabria <j.cabria@creativehope.co.jp>
2020-09-25 23:50:58 +0800

    added footer style and contents

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@ const menuBtn = document.querySelector('.menu');
 const hamburger = document.querySelector('.menu-burger');
 const hamburgerText = document.querySelector('.menu-burger-text');
 const nav = document.querySelector('.nav');
+const backToTop = document.getElementById('backToTop');
 
 let showMenu = false;
 hamburgerText.innerHTML = 'Menu';
@@ -25,3 +26,31 @@ function toggleMenu() {
         showMenu = false;
     }
 }
+
+
+// window.addEventListener('scroll', function() {
+//     if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         backToTop.style.display = 'block';
+//     } else {
+//         backToTop.style.display = 'none';
+//     }
+// })
+
+// backToTop.addEventListener('click', function() {
+//     window.scrollTo(0, 0);
+// })
+
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 20) {
+        $('#backToTop').fadeIn(400);
+    } else {
+        $('#backToTop').fadeOut(400);
+    }
+});
+
+$('#backToTop').click(function() {
+    $('body, html').animate({
+        scrollTop: 0
+    }, 500);
+});
